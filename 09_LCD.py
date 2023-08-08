20d045946c1d00a7378bc44f0509871de8e80083
IshuSahu <ishusahu593@gmail.com>
2023-08-08 21:11:35 +0530

    09-08-23

--- 09_LCD.py
+++ 09_LCD.py
@@ -5,14 +5,14 @@ from datetime import datetime
  
 # Load the driver and set it to "display"
 display = drivers.Lcd()
- 
+
 try:
     print("Writing to display")
     display.lcd_display_string(" WELCOME TO ", 1) # Write line of text to first line of display
     while True:
         # Write line of text to Second line of display
         display.lcd_display_string(" MCA ", 2)
+        
 except KeyboardInterrupt:
-# If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
         print("Cleaning up!")
         display.lcd_clear()
